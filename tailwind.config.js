f0f99c8099652b991b1e052280bc4b8408d3d6c5
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-06-04 21:44:13 +0200

    adding basic structure for react files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "Dark-Blue": " hsl(209, 23%, 22%)",
+        "Very-Dark-Blue": "hsl(207, 26%, 17%)",
+        "Very-Dark-Blue ": "hsl(200, 15%, 8%)",
+        "Dark-Gray": "hsl(0, 0%, 52%)",
+        "Very-Light-Gray": "hsl(0, 0%, 98%)",
+        White: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
